c1d7727d5077664fe41d84064ee888dba3a7eb27
lainjiang <lain.lai.jiang@gmail.com>
2015-07-27 13:53:48 -0500

    Implemented _.extend()

--- src/underbar.js
+++ src/underbar.js
@@ -232,6 +232,12 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i=1; i<arguments.length; i++){
+      _.each(arguments[i],function(item,prop){
+        obj[prop]=item;
+      })
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
